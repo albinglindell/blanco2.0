d3ce8c58cbb1f7d9c99bc856b855a148c5768040
albin lindell <albinglindell98@gmail.com>
2024-06-16 11:05:12 +0200

    quickfix

--- src/pages/profilePage/ProfilePage.jsx
+++ src/pages/profilePage/ProfilePage.jsx
@@ -2,12 +2,34 @@ import { useParams } from 'react-router-dom'
 import artists from '../../assets/artists';
 import { FaInstagram } from "react-icons/fa";
 import Header from '../../components/header/Header';
+import { useEffect, useState } from 'react';
 // import EmailForm from './EmailForm';
 
-const isMobile = window.innerWidth <= 768
+
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return isMobile;
+};
 
 function ProfilePage() {
+  const isMobile = useIsMobile()
     const {name} = useParams()
+  useEffect(()=>{
+
+  },[])
+
     const artist = artists.find( (artistName) => artistName.name === name );
   return (
     <div>
