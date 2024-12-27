28ed247cf667e3fcd39cad07f24bf5974e4b6f9a
albin lindell <albinglindell98@gmail.com>
2024-12-27 16:18:52 +0100

    fixed text

--- src/assets/artists.js
+++ src/assets/artists.js
@@ -42,7 +42,8 @@ import aminaSquare from "../assets/images/squares/amina.jpg"
         "image_sm":AdrianSM,
         "image_md":AdrianMD,
         "email":"adrian@studioblancotattoo.com",
-        "instagram": "adrian.lindell","questions":["Hur skulle du beskriva din tatueringsstil?", " Vad inspirerar dig?" ,"Vad är din bakgrund som tatuerare? "],
+        "instagram": "adrian.lindell",
+        "questions":["Hur skulle du beskriva din tatueringsstil?", " Vad inspirerar dig?" ,"Vad är din bakgrund som tatuerare? "],
         "answers":["”Främst black & grey realism men är öppen för dom flesta tankar och idéer.”",
       "”I mitt designarbete inspireras jag mest av kundens story och berättelser om varför personen just vill ha tatueringen. Då upplever jag att möjligheterna att få ett nytt och häftigt bildspråk kan framkomma.”",
        "”Jag började jobba som tatuerare 2016 och har jobbat ganska mycket runt i världen för att vidga mina vyer och utvecklas i tatuerandet.”"]
@@ -57,12 +58,9 @@ import aminaSquare from "../assets/images/squares/amina.jpg"
         "email":"andy@studioblancotattoo.com",
         "instagram": "andyblancotattoo",
         "questions":["Hur skulle du beskriva din tatueringsstil?", " Vad inspirerar dig?" ,"Vad är din bakgrund som tatuerare? "],
-       
         "answers":["”Jag har specialiserat mig på tatueringar i gråskala! Jag strävar efter ett tidlöst uttryck med en bra balans mellan detaljrikedom och tydliga kompositioner. Både större och mindre projekt är kul!”",
-
       "”Främst inspireras jag av att lyssna på kundens tankar och idéer som utgångspunkt. Eftersom varje samarbete blir unikt, strävar jag efter att skapa en design som speglar just detta. Av erfarenhet blir det oftast som bäst när det finns öppenhet och tillit till att våga skapa något eget.  Utöver min passion för tatueringskonsten hämtar jag inspiration från en mängd olika källor, inklusive klassisk och modern konst, musik, film, litteratur och naturen.”",
-        
-       "” Jag började tatuera 2009 och sedan dess har det alltid varit mitt största intresse. Genom tatuerandet har jag fått möjlighet att resa runt i världen, träffa många olika människor och få ägna mig åt något jag verkligen älskar, vilket jag är tacksam för. År 2016 öppnade jag Studio Blanco Tattoo”"]
+       "”Jag började tatuera 2009 och sedan dess har det alltid varit mitt största intresse. Genom tatuerandet har jag fått möjlighet att resa runt i världen, träffa många olika människor och få ägna mig åt något jag verkligen älskar, vilket jag är tacksam för. År 2016 öppnade jag Studio Blanco Tattoo”"]
     },
     {
         "name":"THOMAS BLOMQVIST",
@@ -74,11 +72,8 @@ import aminaSquare from "../assets/images/squares/amina.jpg"
         "email":"thomas@studioblancotattoo.com",
         "instagram": "atypical.thomas",
         "questions":["Hur skulle du beskriva din tatueringsstil?", " Vad inspirerar dig?" ,"Vad är din bakgrund som tatuerare? "],
-       
         "answers":["”Min stil är black & gray men kommer ifrån gamla skolan där man gjorde allt som trillade in på studion, så jag kan fortfarande uppskatta att göra andra stilar lite då och då.”",
-
       "”Kanske låter klyschigt men min största inspiration är nog att se hur folk lyser när dom går och kollar i spegeln, att få göra nån mer nöjd med sig själv är nått jävligt speciellt.”",
-        
        "”Jag började tatuera som 17 åring”"]
     },
     {
@@ -92,9 +87,7 @@ import aminaSquare from "../assets/images/squares/amina.jpg"
         "instagram": "aminaselling",
         "questions":["Hur skulle du beskriva din tatueringsstil?", " Vad inspirerar dig?" ,"Vad är din bakgrund som tatuerare? "],
         "answers":["”Amina Selling kvinnlig tatuerare. Jobbar specifikt med black and grey”",
-
-      "”Mötet med andra människor är inspirerande. Mina kunders historier och sinnes intryck skapar ett fint möte och den känslan vill jag förmedla på både hud och konst.”",
-        
+        "”Mötet med andra människor är inspirerande. Mina kunders historier och sinnes intryck skapar ett fint möte och den känslan vill jag förmedla på både hud och konst.”",
        "”Min resa som tatuerare började 2018. Vilket var väldigt givande på många sätt! Att jag idag kan jobba som konstnär och tatuera är jag tacksam för!”"]
     }
 ]
